09dadf9ecd0e4654b390950e10df9d2019b42bdb
adityagaikwad888 <adityakg99@gmail.com>
2025-06-29 17:01:05 +0530

    ok

--- backend/server.js
+++ backend/server.js
@@ -26,7 +26,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["http://localhost:3001", "http://localhost:5173"], // Frontend URLs
+    origin: ["http://localhost:3001", "http://65.0.55.149:3001"], // Frontend URLs
     credentials: true, // Allow cookies to be sent
   })
 );
